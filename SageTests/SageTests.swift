a47b157fae20c945dca6cfeff37f879a7c4f61db
Ivy <ivy.marie.hamilton@gmail.com>
2025-07-24 20:17:00 +0100

    Initial Commit

---
+++ SageTests/SageTests.swift
@@ -0,0 +1,16 @@
+//
+//  SageTests.swift
+//  SageTests
+//
+//  Created by Ivy Hamilton on 24/7/2025.
+//
+
+import Testing
+
+struct SageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
